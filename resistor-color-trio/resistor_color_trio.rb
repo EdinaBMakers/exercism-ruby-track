bbab3dfef143c5184a5484b238de186f7e0cef0b
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-11-30 20:58:08 +0000

    Refactor

--- resistor-color-trio/resistor_color_trio.rb
+++ resistor-color-trio/resistor_color_trio.rb
@@ -6,32 +6,24 @@ class ResistorColorTrio
   end
 
   def label
-    validate
+    raise ArgumentError if @colors.any? { |color| !COLORS.include?(color) }
 
     return "Resistor value: #{resistor_value_with_unit}"
   end
 
   private
 
-  def validate
-    @colors.each do |color| 
-      if !COLORS.include?(color)
-        raise ArgumentError
-      end
-    end
-  end
-
   def resistor_value_with_unit
     first_color = @colors[0]
     second_color = @colors[1]
     third_color = @colors[2]
 
-    resistor_value = (10 * color_code(first_color) + color_code(second_color)) * 10 ** color_code(third_color)
+    resistor_value = (10 * color_value(first_color) + color_value(second_color)) * 10 ** color_value(third_color)
     
     return resistor_value / 1000 > 1 ? "#{resistor_value / 1000} kiloohms" : "#{resistor_value} ohms"
   end
 
-  def color_code(color)
+  def color_value(color)
     return COLORS.index(color)
   end
 end
