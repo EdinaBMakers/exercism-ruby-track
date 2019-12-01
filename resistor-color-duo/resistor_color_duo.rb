8d09dc8e4c64ddf4d9e45b877c88a2ffbd643230
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-12-01 11:43:12 +0000

    Refactor

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,11 +1,11 @@
 class ResistorColorDuo
-  COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+  COLORS = %w[black brown red orange yellow green blue violet grey white]
 
   def self.value(colors)
-    return 10 * color_value(colors[0]) + color_value(colors[1])
+    10 * color_value(colors[0]) + color_value(colors[1])
   end
 
   def self.color_value(color)
-    return COLORS.index(color)
+    COLORS.index(color)
   end
 end
